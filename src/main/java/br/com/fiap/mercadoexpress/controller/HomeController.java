1643622990a5ec9684b1e3e07c2e9b8b49a9fdae
luisa-ganasevici <lganasevici@gmail.com>
2026-08-25 19:47:24 -0300

    teste

--- src/main/java/br/com/fiap/mercadoexpress/controller/HomeController.java
+++ src/main/java/br/com/fiap/mercadoexpress/controller/HomeController.java
@@ -11,6 +11,8 @@ import java.util.Map;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+
+//testando
 @RestController
 public class HomeController {
 
