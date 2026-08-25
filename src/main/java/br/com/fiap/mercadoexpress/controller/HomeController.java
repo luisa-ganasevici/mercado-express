e32f123bb2087303f4cc5fb153238532d1b0acf4
luisa-ganasevici <lganasevici@gmail.com>
2026-08-25 19:51:34 -0300

    adicionando securiy

--- src/main/java/br/com/fiap/mercadoexpress/controller/HomeController.java
+++ src/main/java/br/com/fiap/mercadoexpress/controller/HomeController.java
@@ -12,7 +12,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 
-//testando
+
 @RestController
 public class HomeController {
 
