9e1090d3b8c015d78fbd290538c704db73f1b8fa
Luisa Ganasevici <lganasevici@gmail.com>
2026-08-30 01:29:50 -0300

    mercadoWebController - fix

--- src/main/java/br/com/fiap/mercadoexpress/controller/MercadoWebController.java
+++ src/main/java/br/com/fiap/mercadoexpress/controller/MercadoWebController.java
@@ -1,25 +1,21 @@
 package br.com.fiap.mercadoexpress.controller;
 
-
 import br.com.fiap.mercadoexpress.entity.Mercado;
+import br.com.fiap.mercadoexpress.service.MercadoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 import java.util.List;
-import br.com.fiap.mercadoexpress.service.MercadoService;
 
 @RequiredArgsConstructor
 @Controller
-
 public class MercadoWebController {
 
-
     private final MercadoService mercadoService;
-
 
     @GetMapping("/produtos")
     public String getProdutos(Model model) {
@@ -27,9 +23,10 @@ public class MercadoWebController {
         model.addAttribute("getProdutos", listaProdutos);
         return "produtos";
     }
-}
+
     @PostMapping("/produtos")
     public String criarProduto(@ModelAttribute Mercado mercado) {
         mercadoService.criar(mercado);
         return "redirect:/produtos";
+    }
 }
